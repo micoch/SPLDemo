4c04aede1f9236c25d8c5b5ad0b1fc68c79c9a89
Michael Cochran <mcochran0@gmail.com>
2015-12-09 12:12:20 -0500

    Added smooth scrolling

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -1,5 +1,5 @@
 $(window).scroll(function(){
-  if ($(document).scrollTop() > 1){
+  if ($(document).scrollTop() > 5){
     $('#header').addClass('shrink');
     $('#header-text').addClass('move');
     $('body').addClass('body-move');
@@ -8,4 +8,19 @@ $(window).scroll(function(){
     $('#header-text').removeClass('move');
     $('body').removeClass('body-move');
   }
+});
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: (target.offset().top - 325 )
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
