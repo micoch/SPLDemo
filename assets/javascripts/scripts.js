f7805dacbd75846200d4a1209c13438cf3c13168
Michael Cochran <mcochran0@gmail.com>
2015-12-09 20:37:59 -0500

    added smooth scroll

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -1,26 +1,24 @@
-$(window).scroll(function(){
-  if ($(document).scrollTop() > 5){
-    $('#header').addClass('shrink');
-    $('#header-text').addClass('move');
-    $('body').addClass('body-move');
-  } else {
-    $('#header').removeClass('shrink');
-    $('#header-text').removeClass('move');
-    $('body').removeClass('body-move');
-  }
-});
-
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: (target.offset().top - 325 )
-        }, 1000);
-        return false;
-      }
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if ($(document).scrollTop() > 50){
+      $('#header').addClass('shrink');
+      $('#header-text').addClass('move');
+      //$('#body-content').addClass('body-move');
+    } else {
+      $('#header').removeClass('shrink');
+      $('#header-text').removeClass('move');
+      //$('#body-content').removeClass('body-move');
     }
   });
+
+  $('html').niceScroll();
+
+  smoothScroll.init({
+    selectorHeader: '[nav]',
+    speed: 1000,
+    updateURL: false,
+    easing: 'easeInOutCubic',
+    offset: 170
+  });
+
 });
